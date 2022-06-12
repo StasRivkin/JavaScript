6e82700cb047c953722524b5a4bbd10a991c8ad2
Stas <rsl14591@gmail.com>
2022-06-12 20:36:08 +0300

    lost context and asinc

--- script/script.js
+++ script/script.js
@@ -1,14 +1,38 @@
-const library = new Library;
+// //context loosing
+// class User {
+//     constructor(nickname){
+//         this.nickname = nickname;
+//     }
 
-//style="visibility: hidden;"
+//     infoFunc = function (greeting, simbol) {
+//         console.log(`${greeting} Nickname : ${this.nickname}${simbol}`);
+//     }
 
-addBook.onclick = function(){
-    
-    if(isbn.value && title.value && author.value && year.value){
-    const book = new Book(isbn.value.trim(), title.value.trim(), author.value.trim(), year.value);
-    library.addBook(book);
-    }
-    else{
-        alert('all fields must be filled');
-    }  
-}
+//     infoArrow = () => console.log(`Nickname : ${this.nickname}`);
+
+// }
+
+// const peter = new User('Peter');
+// console.log('func');
+// peter.infoFunc('Mr.', '!');
+// console.log('arrow');
+// peter.infoArrow();
+// fn(peter.infoArrow);
+// const john = new User('John');
+// console.log('call, apply');
+// peter.infoFunc.apply(john, ['Mr.', '!']);
+// john.infoFunc.call(peter, 'Mr.', '!');
+
+// function fn(info){
+//     info();
+// }
+
+//asinchron
+console.log('one');
+setTimeout(function(){
+    console.log('two');
+}, 2000);
+setTimeout(function(){
+    console.log('two 1/2');
+}, 1500);
+console.log('three');
